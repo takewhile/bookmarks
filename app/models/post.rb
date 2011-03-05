a6a1ba0f4540727767c7db972b885e34c3149c76
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-03-06 02:05:01 +0530

    Remove slug, use random string as _id for slug.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,17 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Timestamps
+  
   field :title, :type => String
-  field :slug, :type => String
+  identity :type => String
+
+  attr_accessible :title
+
+  before_save :generate_identity
+
+  private
+  def generate_identity
+    require 'active_support/secure_random'
+    self._id = ActiveSupport::SecureRandom.hex(16)
+  end
 end
